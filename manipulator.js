bc96f6e9407b1f3c980a822dea06999cc62c17a2
siannas <massive.annas@gmail.com>
2018-09-25 08:55:24 +0700

    commit master

---
+++ manipulator.js
@@ -0,0 +1,23 @@
+$(function(){
+
+	$("#btn1").click(function(){
+		$(".sample").slideToggle("slow");
+	});
+	
+	$("#btn2").click(function(){
+		var textBox = $(".kabar input:only-child");
+		
+		if(textBox.val() == ""){
+			textBox.css( "border", "2px red solid" );
+		}else{
+			var value = $(".kabar p");
+			value.text("Hai "+textBox.val());
+			textBox.css( "border", "2px blue solid" );
+		}
+		
+	});
+	
+	$("#btn3").click(function(){
+		
+	});
+});
